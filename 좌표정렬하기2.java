68c316f63c34236a92902a358eacb18f31b94253
kkimkitae <85480964+kkimkitae@users.noreply.github.com>
2021-09-26 17:32:34 +0900

    Add files via upload

---
+++ 좌표정렬하기2.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args)  {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int input = scanner.nextInt();
+		
+		int [][] number = new int[input][2];
+		
+		for ( int i =0; i<input; i++) {
+			number[i][0] = scanner.nextInt();
+			number[i][1] = scanner.nextInt();
+		}
+		Arrays.sort(number, new Comparator<int[]> () {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				// TODO Auto-generated method stub
+				if(o1[1] == o2[1]) {
+					return Integer.compare(o1[0],o2[0]);
+				}
+				else {
+					return Integer.compare(o1[1], o2[1]);
+				}
+			}
+		});
+		
+		for (int i=0; i<input; i++) {
+			System.out.println(number[i][0]+" "+number[i][1]);
+		}
+	}
+	}
+
